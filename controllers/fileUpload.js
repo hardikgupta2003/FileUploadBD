0ba2ef7b28bb44b1c5efff44c9999e17072ea199
hardikgupta2003 <hardikgupta5172@gmail.com>
2024-01-31 00:06:30 +0530

    added image upload functionality

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -8,7 +8,7 @@ exports.localFileUpload = async (req, res) => {
 
         //fetch filefrom request
         const file = req.files.file;
-        console.log("FILE AAGYI JEE -> ",file);
+        console.log("FILE AAGYI JEE -> ", file);
 
 
         //create path where file need to be stored on server
@@ -16,80 +16,80 @@ exports.localFileUpload = async (req, res) => {
         console.log("PATH-> ", path)
 
         //add path to the move fucntion
-        file.mv(path , (err) => {
+        file.mv(path, (err) => {
             console.log(err);
         });
 
         //create a successful response
         res.json({
-            success:true,
-            message:'Local File Uploaded Successfully',
+            success: true,
+            message: 'Local File Uploaded Successfully',
         });
 
     }
-    catch(error) {
+    catch (error) {
         console.log("Not able to upload the file on server")
         console.log(error);
     }
 }
 
-async function uploadFileToCloudinary(file,folder){
-    const options = {folder};
-    return await cloudinary.uploader.upload(file.tempFilePath);
-    
+async function uploadFileToCloudinary(file, folder) {
+    const options = { folder };
+    console.log("temp file path : ", file.tempFilePath)
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
+function isFileTypeSupported(type, supportedTypes) {
+    return supportedTypes.includes(type);
 }
 
 //image upload handler
-exports.imageUpload=async (req,res,next)=>{
-    try{
+exports.imageUpload = async (req, res) => {
+    try {
         // data fetch 
-        const {name,tags,email}=req.body;
-        console.log(name,tags,email);
+        const { name, tags, email } = req.body;
+        console.log(name, tags, email);
 
         const file = req.files.imageFile;
         console.log(file);
 
         //validation
-        const supportedTypes =  ['image/jpeg','image/jpg','image/png'];
-        if(!supportedTypes.includes(file.mimetype)){
-            return res.status(400).send('Invalid image type');
-        }
+        const supportedTypes = ["jpeg", "jpg", "png"];
+        console.log(file.name)
 
-        const fileType=file.name.split('.')[1].toLowerCase();
-        if(fileType!=='jpg' && fileType !== 'png'){
-           throw new Error('Only jpg and png images are allowed');
-       }
-       const response = await uploadFileToCloudinary(file,"hardik")
 
-       return res.status(400).json({
-        success:true,
-        message:"Image uploaded successfully",
-       })
-
-       //save image in database
-    //    let base64Data=await getBase64(file.path);
-    //    await ImageModel.addImageToDB(name,base64Data,tags,email);
-
-    //    //delete local file after saving it into db
-    //    fs.unlinkSync(file.path);
-
-    //    next()
-   }catch(err){
-      console.log(err);
-      res.status(500).json({message: err.toString()});
-   }
-};
-
-// function getBase64(imgPath, callback) {
-//     fs.readFile(imgPath, {encoding: 'base64'}, function(err, data) {
-//         if (!err) {
-//             callback(null, data);
-//         } else {
-//             callback(err);
-//         }
-//     });
-// }
-
-module.exports={uploadImage};
-
+        const fileType = file.name.split('.')[1].toLowerCase();
+        console.log("file type : ", fileType)
         
+        if (!isFileTypeSupported(fileType, supportedTypes)) {
+            return res.status(400).json({
+                success: false,
+                error: "Invalid Image Type"
+            })
+        };
+
+
+        const response = await uploadFileToCloudinary(file, "hardik");
+        console.log(response);
+
+        //db entry
+        const fileData = await File.create({
+            name,
+            imageUrl: response.secure_url,
+            tags,
+            email,
+        })
+
+        return res.status(500).json({
+            success: true,
+            imageUrl: response.secure_url,
+            message: "Image uploaded successfully",
+        })
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: 'something went Wrong',
+        });
+    }
+};
