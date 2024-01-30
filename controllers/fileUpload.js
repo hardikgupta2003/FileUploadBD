0620cd9a8284ca6af9acdf68aa70ef775f3d675a
hardikgupta2003 <hardikgupta5172@gmail.com>
2024-01-30 19:32:09 +0530

    cloudinary added

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,4 +1,5 @@
 const File = require("../models/File");
+const cloudinary = require("cloudinary");
 
 //localfileupload -> handler function
 
@@ -31,3 +32,64 @@ exports.localFileUpload = async (req, res) => {
         console.log(error);
     }
 }
+
+async function uploadFileToCloudinary(file,folder){
+    const options = {folder};
+    return await cloudinary.uploader.upload(file.tempFilePath);
+    
+}
+
+//image upload handler
+exports.imageUpload=async (req,res,next)=>{
+    try{
+        // data fetch 
+        const {name,tags,email}=req.body;
+        console.log(name,tags,email);
+
+        const file = req.files.imageFile;
+        console.log(file);
+
+        //validation
+        const supportedTypes =  ['image/jpeg','image/jpg','image/png'];
+        if(!supportedTypes.includes(file.mimetype)){
+            return res.status(400).send('Invalid image type');
+        }
+
+        const fileType=file.name.split('.')[1].toLowerCase();
+        if(fileType!=='jpg' && fileType !== 'png'){
+           throw new Error('Only jpg and png images are allowed');
+       }
+       const response = await uploadFileToCloudinary(file,"hardik")
+
+       return res.status(400).json({
+        success:true,
+        message:"Image uploaded successfully",
+       })
+
+       //save image in database
+    //    let base64Data=await getBase64(file.path);
+    //    await ImageModel.addImageToDB(name,base64Data,tags,email);
+
+    //    //delete local file after saving it into db
+    //    fs.unlinkSync(file.path);
+
+    //    next()
+   }catch(err){
+      console.log(err);
+      res.status(500).json({message: err.toString()});
+   }
+};
+
+// function getBase64(imgPath, callback) {
+//     fs.readFile(imgPath, {encoding: 'base64'}, function(err, data) {
+//         if (!err) {
+//             callback(null, data);
+//         } else {
+//             callback(err);
+//         }
+//     });
+// }
+
+module.exports={uploadImage};
+
+        
