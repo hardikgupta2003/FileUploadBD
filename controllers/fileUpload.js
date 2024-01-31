74fcfef1dbef567dac64d1bdf7ac5ac0fe00dadb
hardikgupta2003 <hardikgupta5172@gmail.com>
2024-01-31 11:21:10 +0530

    Added Video Upload Feature

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,5 +1,6 @@
 const File = require("../models/File");
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
+
 
 //localfileupload -> handler function
 
@@ -8,7 +9,7 @@ exports.localFileUpload = async (req, res) => {
 
         //fetch filefrom request
         const file = req.files.file;
-        console.log("FILE AAGYI JEE -> ", file);
+        console.log("FILE AAGYI JEE -> ",file);
 
 
         //create path where file need to be stored on server
@@ -16,80 +17,190 @@ exports.localFileUpload = async (req, res) => {
         console.log("PATH-> ", path)
 
         //add path to the move fucntion
-        file.mv(path, (err) => {
+        file.mv(path , (err) => {
             console.log(err);
         });
 
         //create a successful response
         res.json({
-            success: true,
-            message: 'Local File Uploaded Successfully',
+            success:true,
+            message:'Local File Uploaded Successfully',
         });
 
     }
-    catch (error) {
+    catch(error) {
         console.log("Not able to upload the file on server")
         console.log(error);
     }
 }
 
-async function uploadFileToCloudinary(file, folder) {
-    const options = { folder };
-    console.log("temp file path : ", file.tempFilePath)
-    return await cloudinary.uploader.upload(file.tempFilePath, options);
-}
 function isFileTypeSupported(type, supportedTypes) {
     return supportedTypes.includes(type);
 }
 
-//image upload handler
+async function uploadFileToCloudinary(file, folder, quality) {
+    const options = {folder};
+    console.log("temp file path", file.tempFilePath);
+
+    if(quality) {
+        options.quality = quality;
+    }
+
+    options.resource_type = "auto";
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
+
+//image upload ka hadnler
 exports.imageUpload = async (req, res) => {
-    try {
-        // data fetch 
-        const { name, tags, email } = req.body;
-        console.log(name, tags, email);
+    try{
+        //data fetch
+        const { name, tags, email} = req.body;
+        console.log(name,tags,email);
 
         const file = req.files.imageFile;
         console.log(file);
 
-        //validation
-        const supportedTypes = ["jpeg", "jpg", "png"];
-        console.log(file.name)
-
-
+        //Validation
+        const supportedTypes = ["jpg", "jpeg", "png"];
         const fileType = file.name.split('.')[1].toLowerCase();
-        console.log("file type : ", fileType)
-        
-        if (!isFileTypeSupported(fileType, supportedTypes)) {
+        console.log("File Type:", fileType);
+
+        if(!isFileTypeSupported(fileType, supportedTypes)) {
             return res.status(400).json({
-                success: false,
-                error: "Invalid Image Type"
+                success:false,
+                message:'File format not supported',
             })
-        };
+        }
 
-
-        const response = await uploadFileToCloudinary(file, "hardik");
+        //file format supported hai
+        
+        const response = await uploadFileToCloudinary(file, "Personal");
         console.log(response);
 
-        //db entry
+        //db me entry save krni h
         const fileData = await File.create({
             name,
-            imageUrl: response.secure_url,
             tags,
             email,
-        })
-
-        return res.status(500).json({
-            success: true,
-            imageUrl: response.secure_url,
-            message: "Image uploaded successfully",
-        })
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            success: false,
-            message: 'something went Wrong',
+            imageUrl:response.secure_url,
         });
+
+        res.json({
+            success:true,
+            imageUrl:response.secure_url,
+            message:'Image Successfully Uploaded',
+        })
     }
-};
+    catch(error) {
+        console.error(error);
+        res.status(400).json({
+            success:false,
+            message:'Something went wrong',
+        });
+
+    }
+}
+
+//video upload ka handler
+
+exports.videoUpload = async (req,res) => {
+    try{
+        //data fetch
+        const { name, tags, email} = req.body;
+        console.log(name,tags,email);
+        
+        const file = req.files.videoFile;
+
+         //Validation
+         const supportedTypes = ["mp4", "mov"];
+         const fileType = file.name.split('.')[1].toLowerCase();
+         console.log("File Type:", fileType);
+ 
+         //TODO: add a upper limit of 5MB for Video
+         if(!isFileTypeSupported(fileType, supportedTypes)) {
+             return res.status(400).json({
+                 success:false,
+                 message:'File format not supported',
+             })
+         }
+
+          //file format supported hai
+        console.log("Uploading ...");
+        const response = await uploadFileToCloudinary(file, "Personal");
+        console.log(response);
+
+        //db me entry save krni h
+        const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl:response.secure_url,
+        });
+
+        res.json({
+            success:true,
+            imageUrl:response.secure_url,
+            message:'Video Successfully Uploaded',
+        })
+
+    }
+    catch(error) {
+        console.error(error);
+        res.status(400).json({
+            success:false,
+            message:'Something went wrong',
+        })
+    }
+}
+
+//imageSizeReducer
+exports.imageSizeReducer = async (req,res) => {
+    try{
+        //data fetch
+        const { name, tags, email} = req.body;
+        console.log(name,tags,email);
+
+        const file = req.files.imageFile;
+        console.log(file);
+
+        //Validation
+        const supportedTypes = ["jpg", "jpeg", "png"];
+        const fileType = file.name.split('.')[1].toLowerCase();
+        console.log("File Type:", fileType);
+
+        //TODO: add a upper limit of 5MB for Video
+        if(!isFileTypeSupported(fileType, supportedTypes)) {
+            return res.status(400).json({
+                success:false,
+                message:'File format not supported',
+            })
+        }
+
+        //file format supported hai
+        console.log("Uploading to Codehelp");
+        //TODO: height attribute-> COMPRESS
+        const response = await uploadFileToCloudinary(file, "Codehelp", 90);
+        console.log(response);
+
+        //db me entry save krni h
+        const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl:response.secure_url,
+        });
+
+        res.json({
+            success:true,
+            imageUrl:response.secure_url,
+            message:'Image Successfully Uploaded',
+        })
+    }
+    catch(error) {
+        console.error(error);
+        res.status(400).json({
+            success:false,
+            message:'Something went wrong',
+        })
+    }
+}
